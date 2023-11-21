838e29cc252b5d9fd87de39f4ee275a95cfb1eb3
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-11-21 10:00:57 -0500

    Fix container name and add job definition tag

--- deploy/terraform/confluence-moi.tf
+++ deploy/terraform/confluence-moi.tf
@@ -4,7 +4,7 @@ resource "aws_batch_job_definition" "generate_batch_jd_moi" {
   type                  = "container"
   container_properties  = <<CONTAINER_PROPERTIES
   {
-    "image": "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/moi",
+    "image": "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-moi",
     "executionRoleArn": "${data.aws_iam_role.exe_role.arn}",
     "jobRoleArn": "${data.aws_iam_role.job_role.arn}",
     "fargatePlatformConfiguration": { "platformVersion": "LATEST" },
@@ -65,4 +65,5 @@ resource "aws_batch_job_definition" "generate_batch_jd_moi" {
    retry_strategy {
     attempts = 3
   }
+  tags = { "job_definition": "${var.prefix}-moi" }
 }
