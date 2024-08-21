5259bd16606b79e922e96f33d7b58ec469c29cc7
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2024-08-21 16:41:50 +0000

    Implement commandline arguments

--- run_MOI.py
+++ run_MOI.py
@@ -2,6 +2,7 @@
 """
 
 # Standard imports
+import argparse
 import json
 import os
 from pathlib import Path
@@ -169,36 +170,64 @@ def set_moi_params():
     return moi_params
 
 
-def main():
+def create_args():
+    """Create and return argparsers with command line arguments."""
+    
+    arg_parser = argparse.ArgumentParser(description='Integrate FLPE')
+    arg_parser.add_argument('-i',
+                            '--index',
+                            type=int,
+                            help='Index to specify input data to execute on')
+    arg_parser.add_argument('-j',
+                            '--basinjson',
+                            type=str,
+                            help='Name of the basin.json',
+                            default='basin.json')
+    arg_parser.add_argument('-v',
+                            '--verbose',
+                            help='Indicates verbose logging',
+                            action='store_true')
+    arg_parser.add_argument('-b',
+                            '--branch',
+                            type=str,
+                            help='Indicates constrained or unconstrained run',
+                            choices=['constrained', 'unconstrained'],
+                            default='unconstrained')
+    arg_parser.add_argument('-s',
+                            '--sosbucket',
+                            type=str,
+                            help='Name of the SoS bucket and key to download from',
+                            default='')
+    return arg_parser
 
-    print('basin file:',sys.argv[1])
-    print('verbose flag:',sys.argv[2])
-    print('branch:',sys.argv[3])
+
+def main():
+    
+    # commandline arguments
+    arg_parser = create_args()
+    args = arg_parser.parse_args()
+
+    print('index: ', args.index)
+    print('basin file: ', args.basinjson)
+    print('verbose flag: ', args.verbose)
+    print('branch: ', args.branch)
+    print('sosbucket: ', args.sosbucket)
+    
     try:
         print('index:',sys.argv[4])
     except:
         print('running on AWS index')
 
     # verbose 
-    try: 
-        VerboseFlag=sys.argv[2]
-        if VerboseFlag == '-v': 
-            Verbose=True
-        else:
-            Verbose=False
-    except IndexError:
+    if args.verbose:
+        Verbose=True
+    else:
         Verbose=False
 
-    #branch
-    try:
-        Branch=sys.argv[3]
-    except IndexError:
-        Branch='unconstrained'
-
     #context
-    try:
-        index_to_run=int(sys.argv[4]) #integer
-    except IndexError:
+    if args.index:
+        index_to_run=args.index
+    else:
         index_to_run=-235
 
     #data directories
@@ -214,12 +243,9 @@ def main():
         OUTPUT_DIR = basedir.joinpath("moi")
 
     #basin data
-    try:
-        basin_json = INPUT_DIR.joinpath(sys.argv[1]) #turn this on for standard operations: AWS or running default basin file
-        #basin_json = Path("/home/mdurand_umass_edu/dev-confluence/mnt/").joinpath(sys.argv[1]) #turn this on to use a local basin file
-        print('Using',basin_json)           
-    except IndexError:
-        basin_json = INPUT_DIR.joinpath("basin.json") 
+    basin_json = INPUT_DIR.joinpath(args.basinjson) #turn this on for standard operations: AWS or running default basin file
+    #basin_json = Path("/home/mdurand_umass_edu/dev-confluence/mnt/").joinpath(args.basinjson) #turn this on to use a local basin file
+    print('Using',basin_json)           
 
     basin_data = get_basin_data(basin_json,index_to_run)
 
